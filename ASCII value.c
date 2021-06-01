963b4433d66d87c501c73f262efae61b0d65b7d5
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-06-01 19:54:45 +0530

    Add files via upload

---
+++ ASCII value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char chr;
+	printf("enter the character:");
+	scanf("%c",&chr);
+	printf("you entered %c\n",chr);
+	//when %d is used ASCII value is displayed
+	printf("ASCII Value is %d",chr);
+	return 0;
+}
