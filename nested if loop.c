3eaf2d50f3a4cd984eafefb6a4dce724785017d1
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-06-04 21:38:05 +0530

    Add files via upload

---
+++ nested if loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the numbers:");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		printf("%d is big",a);
+	    else
+		printf("%d is big",c);
+	}
+	else
+	{
+	if(b>c)
+		printf("%d is big",b);
+	    else
+		printf("%d is big",c);
+	}
+}
